7553a13d51310160be64d8467af5ce936f7a46b3
SlavaTelyatko <telyatko.slava@docuchain.io>
2024-06-19 14:20:28 +0300

    fix add sign email template

--- src/main.ts
+++ src/main.ts
@@ -25,15 +25,16 @@ async function bootstrap() {
 
   app.enableCors();
 
-  const config = new DocumentBuilder()
-    .setTitle('Docuchain API')
-    .setDescription('The docuchain API description')
-    .setVersion('1.0')
-    .build();
+  if (process.env.NODE_ENV !== 'production') {
+    const config = new DocumentBuilder()
+      .setTitle('Docuchain API')
+      .setDescription('The docuchain API description')
+      .setVersion('1.0')
+      .build();
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+  }
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
