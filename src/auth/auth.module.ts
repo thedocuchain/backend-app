a09d3d6fae982e42e4d91112a8b25105932b5ca2
SlavaTelyatko <telyatko.slava@docuchain.io>
2024-06-21 11:27:44 +0300

    add token generation partially, add image check on pdf

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,12 +3,10 @@ import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
 import { UsersModule } from '../users/users.module';
 import { JwtStrategy } from './strategies/jwt.strategy';
-import { AuthController } from './auth.controller';
 
 @Module({
   providers: [AuthService, JwtStrategy],
   imports: [UsersModule, JwtModule],
-  controllers: [AuthController],
   exports: [AuthService, JwtModule],
 })
 export class AuthModule {}
--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
 import { NotificationsController } from './notifications.controller';
 import { UsersModule } from '../users/users.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, AuthModule],
   providers: [NotificationsService],
   exports: [NotificationsService],
   controllers: [NotificationsController],
