9541782a62d65d651748a04ad1f05134ecd277e5
SlavaTelyatko <telyatko.slava@docuchain.io>
2024-06-25 12:05:30 +0300

    add websocket emit documents update events

--- src/documents/dto/update-document.dto.ts
+++ src/documents/dto/update-document.dto.ts
@@ -1,32 +1,4 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsArray, IsNotEmpty, IsString, ValidateNested } from 'class-validator';
-import { ReadUserDto } from '../../users/dto/read-user.dto';
+import { PartialType } from '@nestjs/swagger';
+import { ReadDocumentDto } from './read-document.dto';
 
-export class UpdateDocumentDto {
-  @ApiProperty({
-    description: 'Document name',
-    example: 'Contract',
-  })
-  @IsString()
-  @IsNotEmpty({ message: 'Document name is required' })
-  readonly name: string;
-  @ApiProperty({
-    example: [
-      {
-        name: 'John Smith',
-        email: 'john.smith@gmail.com',
-        role: 'watcher',
-        position: 1,
-      },
-      {
-        name: 'Bob Rider',
-        email: 'bob.rider@gmail.com',
-        role: 'signer',
-        position: 2,
-      },
-    ],
-  })
-  @IsArray()
-  @ValidateNested()
-  readonly users: ReadUserDto[];
-}
+export class UpdateDocumentDto extends PartialType(ReadDocumentDto) {}
